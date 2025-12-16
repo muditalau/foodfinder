e9e44be20c281d568f70565c534cbdccc8335c3a
muditalau <lau.mudita@gmail.com>
2025-12-16 20:55:05 +0700

    Add foodData array with restaurant information

---
+++ js/data.js
@@ -0,0 +1,15 @@
+const foodData = [
+  {
+    name: "Nasi Goreng A",
+    lat: -6.2,
+    lng: 106.8,
+    rating: 4.5
+  },
+  {
+    name: "Bakso B",
+    lat: -6.21,
+    lng: 106.81,
+    rating: 4.2
+  }
+];
+export default foodData;
